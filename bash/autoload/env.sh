88e9bdc34346c9255b6165015b738b8b44e5c28c
thisago <thisago@disroot.org>
2025-06-04 07:56:03 -0300

    docs: pass
    Also add openai api base to redirect to copilot

--- bash/autoload/env.sh
+++ bash/autoload/env.sh
@@ -2,3 +2,6 @@
 
 # https://github.com/dom96/choosenim/blob/master/analytics.md
 export DO_NOT_TRACK=1
+
+# We always use Copilot Proxy for OpenAI API calls
+export OPENAI_API_BASE="http://localhost:8923"
