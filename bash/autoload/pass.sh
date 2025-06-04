bd54e2d1b744ba1bb8c83284488b1a93eeb30c85
thisago <thisago@disroot.org>
2025-06-04 08:08:10 -0300

    feat(bash/autorun): pass completion

---
+++ bash/autoload/pass.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Check if 'pass' command is available
+command -v pass &> /dev/null || return
+
+# Set Pass shell completion file path
+PASS_COMPLETION_FILE="/usr/share/bash-completion/completions/pass"
+
+# Load Pass shell completion if available
+if [ -f "$PASS_COMPLETION_FILE" ]; then
+  source "$PASS_COMPLETION_FILE"
+fi
