a654e78fddd08e2640836594a8ede5dcd313a9b5
thisago <thisago@disroot.org>
2025-06-10 07:49:01 -0300

    refactor(bash/aliases): move functions to utils

---
+++ bash/autoload/utils.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+
+# Create a directory and change into it
+mkcd() {
+  dir="$1"
+  # Check if the argument is provided
+  if [[ -z "$dir" ]]; then
+    echo "Usage: mkcd <directory_name>"
+    return 1
+  fi
+
+  # Create the directory ignoring errors if it already exists
+  mkdir -p "$dir"
+
+  cd "$dir"
+  if [[ $? -ne 0 ]]; then
+    echo "Failed to create or change into directory: $dir"
+    return 1
+  fi
+}
+
+# Create a new file and auto create the directory if not exist
+touchp() {
+  file="$1"
+  # Check if the argument is provided
+  if [[ -z "$file" ]]; then
+    echo "Usage: touchp <file_path>"
+    return 1
+  fi
+
+  # Check if the file exists
+  if [[ -e "$file" ]]; then
+    echo "File already exists: $file"
+    return 1
+  fi
+
+  # Extract file path to create the directory if it doesn't exist
+  dir="$(dirname "$file")"
+
+  # Create the directory if it doesn't exist
+  if [[ ! -d "$dir" ]]; then
+    mkdir -p "$dir"
+  fi
+
+  # Create the file
+  touch "$file"
+}
