181e1a815d72cb46206e07e66e4c0765c6fd0d27
thisago <thisago@disroot.org>
2025-07-23 09:26:00 -0300

    feat(aider/system): user tasks manipulation

--- internal/bash/autoload/libretranslate.sh
+++ internal/bash/autoload/libretranslate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Set environment variables for LibreTranslate
-export LIBRETRANSLATE_HOST="https://translate.disroot.org/"
+export LIBRETRANSLATE_HOST='https://translate.disroot.org/'
 export LIBRETRANSLATE_DEFAULT_LANG='en'
