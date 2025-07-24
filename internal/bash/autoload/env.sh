09ecc3378350cc465a5a017549a5595a63ef90fb
thisago <thisago@disroot.org>
2025-07-24 13:45:00 -0300

    chore(internal/bash/env): commit prompt

--- internal/bash/autoload/env.sh
+++ internal/bash/autoload/env.sh
@@ -70,7 +70,7 @@ The description is a quick description describing the overall intention followed
   Why? Missing quick description, one liner, empty explanations and hard to read.
 
 # Critical Review
-Provide a bullet list with critical key points about the diff. Omit when not needed.
+Provide a bullet list with critical key points about the diff, raising issues. Omit when not needed.
 
 # Overall Commit Message Structure
 The commit message should be structured as follows:
@@ -81,8 +81,8 @@ The commit message should be structured as follows:
 
     ---
 
-    Critical Review:
-    [critical review]
+    Possible Issues:
+    [list of issues]
 
 # Edge Cases
 - If multiple intentions and scopes are present, use the most relevant one in the summary and but include all in the body.
