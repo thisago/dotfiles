06a3c3d42a520ad47eb302d4b6dc407a14d3d857
thisago <thisago@disroot.org>
2025-07-27 20:21:39 -0300

    fix(bash/g): remove unintended skip

--- internal/bash/autoload/g.sh
+++ internal/bash/autoload/g.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Go version manager
 
-# Check if 'g' command is available
-command -v g &> /dev/null || return
-
 # Set environment variables for Go
 export GOROOT="${HOME}/.g/go"
 export G_MIRROR=https://golang.google.cn/dl/
