1cd844fcc352bd2f994697a1ebea129967fc3b19
thisago <thisago@disroot.org>
2025-07-28 17:23:42 -0300

    fix(bash/g): source env if exists

--- internal/bash/autoload/g.sh
+++ internal/bash/autoload/g.sh
@@ -4,3 +4,8 @@
 # Set environment variables for Go
 export GOROOT="${HOME}/.g/go"
 export G_MIRROR=https://golang.google.cn/dl/
+
+# Source env file if it exists
+if [[ -f "${HOME}/.g/env" ]]; then
+  source "${HOME}/.g/env"
+fi
