dd99c513ea73eee173290427885911e1d0a64513
thisago <thisago@disroot.org>
2025-08-01 22:04:26 -0300

    fix(tmux): hide window name when not available
    And move it to the status top

--- internal/tmux/status-top.sh
+++ internal/tmux/status-top.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
-# Prints the hostname and the intention of current session.
+# Prints the hostname, the name of current focused window and the intention of current session.
 
-workflowsDir=~/.workflows/
-if test -d "$workflowsDir"; then
-  sessionFile="$workflowsDir/session.json"
-  sessionPretension="$(cat $sessionFile | jq -r .pretend)"
+echo -n " $HOSTNAME"
 
-  echo -ne " $HOSTNAME 󱞁 $sessionPretension"
+sessionFile=~/.workflows/session.json
+if test -f "$sessionFile"; then
+  sessionPretension=$(jq -r .pretend "$sessionFile")
+  echo -n " 󱞁 $sessionPretension"
+fi
+
+IGNORED_APPS=(
+  '"Firefox" "org.mozilla.firefox"'
+  '"org.mozilla.firefox" "org.mozilla.firefox"'
+  '"gnome-shell" "Gnome-shell"'
+  '"mutter-x11-frames" "mutter-x11-frames"'
+  '"ibus-x11" ""'
+  ''
+)
+
+IGNORED_APPS_REGEX="$(
+  IFS="|"
+  echo -n "${IGNORED_APPS[*]}"
+)"
+
+rawWindows=$(xwininfo -root -children | grep -vE "\(($IGNORED_APPS_REGEX)\)" | grep -E '^ *0x')
+
+if [[ "$(wc -l <<<"$rawWindows")" -gt 1 ]]; then
+  windowName="$(echo "$rawWindows" |
+    tail -n1 |
+    sed -r 's/^ *(0x[^)]+) "([^"]+)": \(("([^"]*)" ?){2}?\) +([0-9]+x[0-9]+)(\+-?[0-9]+\+-?[0-9]+ *){2}/\2 (\5)/')"
+
+  echo -n "  $windowName"
 fi
