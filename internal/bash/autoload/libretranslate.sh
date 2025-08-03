399365df9b3d55cbad757a4c8b3346d6f7e47e43
thisago <thisago@disroot.org>
2025-08-03 14:49:05 -0300

    fix(bash/libretranslate): trailing slash from host

--- internal/bash/autoload/libretranslate.sh
+++ internal/bash/autoload/libretranslate.sh
@@ -3,6 +3,6 @@
 # Set environment variables for LibreTranslate if not set
 
 [[ ! -n "$LIBRETRANSLATE_HOST" ]] && \
-  export LIBRETRANSLATE_HOST='https://translate.disroot.org/'
+  export LIBRETRANSLATE_HOST='https://translate.disroot.org'
 [[ ! -n "$LIBRETRANSLATE_DEFAULT_LANG" ]] && \
   export LIBRETRANSLATE_DEFAULT_LANG='en'
