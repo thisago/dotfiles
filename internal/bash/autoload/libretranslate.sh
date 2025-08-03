d0026179a9fb56077e554fd0f4437a759fa03271
thisago <thisago@disroot.org>
2025-08-03 13:28:43 -0300

    fix(bash/libretranslate): support custom env

--- internal/bash/autoload/libretranslate.sh
+++ internal/bash/autoload/libretranslate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Set environment variables for LibreTranslate
-export LIBRETRANSLATE_HOST='https://translate.disroot.org/'
-export LIBRETRANSLATE_DEFAULT_LANG='en'
+# Set environment variables for LibreTranslate if not set
+
+[[ ! -n "$LIBRETRANSLATE_HOST" ]] && \
+  export LIBRETRANSLATE_HOST='https://translate.disroot.org/'
+[[ ! -n "$LIBRETRANSLATE_DEFAULT_LANG" ]] && \
+  export LIBRETRANSLATE_DEFAULT_LANG='en'
