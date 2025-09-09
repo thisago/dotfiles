9461b7aea89616d000bc84927f66da5d8e5ba4de
thisago <thisago@disroot.org>
2025-09-09 09:43:25 -0300

    fix(firefox/user-overrides): keep history and logins

--- dot_mozilla/firefox/default-release/user-overrides.js
+++ dot_mozilla/firefox/default-release/user-overrides.js
@@ -1,2 +1,11 @@
 user_pref("places.history.enabled", true);
 user_pref("browser.startup.homepage", "about:serviceworkers");
+
+// Keep history
+// See: https://github.com/arkenfox/user.js/issues/1971#issuecomment-3065016884
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.cpd.history", false);
+user_pref("privacy.clearOnShutdown.history", false); // v1 clearOnShutdown
+user_pref("privacy.sanitize.clearOnShutdown.hasMigratedToNewPrefs2", false); // keeps v1 clearOnShutdown used until v2 is stable
+user_pref("privacy.clearOnShutdown_v2.historyFormDataAndDownloads", false); // v2 clearOnShutdown
+user_pref("privacy.clearOnShutdown_v2.siteSettings", false);
