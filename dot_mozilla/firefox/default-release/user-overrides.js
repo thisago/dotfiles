548dcfdeef98159c23f37dd019359b44e7d28805
thisago <thisago@disroot.org>
2025-09-09 09:50:27 -0300

    fix(firefox/user-overrides): keep history
    Still not working

--- dot_mozilla/firefox/default-release/user-overrides.js
+++ dot_mozilla/firefox/default-release/user-overrides.js
@@ -2,6 +2,9 @@ user_pref("places.history.enabled", true);
 user_pref("browser.startup.homepage", "about:serviceworkers");
 
 // Keep history
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", false);
+user_pref("privacy.clearSiteData.historyFormDataAndDownloads", false);
+user_pref("privacy.clearSiteData.browsingHistoryAndDownloads", false);
 // See: https://github.com/arkenfox/user.js/issues/1971#issuecomment-3065016884
 user_pref("privacy.clearOnShutdown.history", false);
 user_pref("privacy.cpd.history", false);
