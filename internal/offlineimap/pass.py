1ff97801a1db3b2702398e7532abba288a412d7c
thisago <thisago@disroot.org>
2025-10-16 19:16:37 -0300

    fix(offlineimap): login

--- internal/offlineimap/pass.py
+++ internal/offlineimap/pass.py
@@ -2,8 +2,8 @@
 from subprocess import check_output
 
 
-def get_pass(account_dir):
+def get_pass(email_dir: str, account_dir: str) -> str:
     """Retrieve the password for the given account name using pass."""
-    command = "pass {{ .pass_email }}/%s/password" % account_dir
+    command = "pass %s/%s/password" % (email_dir, account_dir)
     result = check_output(command, shell=True).strip().decode("utf-8")
     return result
