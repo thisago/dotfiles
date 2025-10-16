a9ba1bff9765626fe873f24b0f5631cdeb458268
thisago <thisago@disroot.org>
2025-10-16 18:56:29 -0300

    feat: vi-mongo and smart email setup

--- internal/offlineimap/pass.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from subprocess import check_output
-
-
-def get_pass(account_name):
-    """Retrieve the password for the given account name using pass."""
-    command = "pass email/%s_pass" % account_name
-    result = check_output(command, shell=True).strip().decode('utf-8')
-    return result
